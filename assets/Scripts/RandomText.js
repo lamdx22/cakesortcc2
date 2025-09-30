85465a74ea9c8da829b2300364168c581a29cd33
Lam Dang <lamdx@falcongames.com>
2025-09-30 20:25:03 +0700

    cake sort text complete

--- assets/Scripts/RandomText.js
+++ assets/Scripts/RandomText.js
@@ -7,12 +7,19 @@ cc.Class({
         textFX: cc.Label,
         listText: [cc.String],
         colors: [cc.Color],
+        percentAppear: 50,
     },
 
     // onLoad () {},
 
     onEnable () {
         if (GameManager.instance.useTextFX) {
+            let random = Math.random()*100;
+            if (random > this.percentAppear) 
+            {
+                this.textFX.string = "";
+                return;
+            }
             let k = Math.floor(Math.random() * this.listText.length);
             if (k > this.listText.length - 1) {
                 k = this.listText.length - 1;
@@ -26,11 +33,15 @@ cc.Class({
             n.scale = 0;
 
             cc.tween(n)
-                .delay(0.15)
-                .to(0.3, { scale: 0.08 }, { easing: "backOut" }) 
+                .delay(0)
+                .parallel(
+                    cc.tween().to(0.7, { scale: 0.06 }, { easing: "backOut" }),
+                    cc.tween().to(0.7, { opacity: 0 }, { easing: "backIn" })
+                )
+                //.to(0.6, { scale: 0.08 }, { easing: "backOut" }) 
                 //.to(0.1, {scale: 0.1}, {easing: "backIn"})
-                .delay(0.5)                                    
-                .to(0.3, { scale: 0, opacity: 0 }, { easing: "backIn" }) 
+                //.delay(0.1)                                    
+                //.to(0.6, { scale: 0, opacity: 0 }, { easing: "backIn" }) 
                 .start();
         } else {
             this.textFX.string = "";
